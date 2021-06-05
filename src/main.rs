a97bb8010cc5136cf88a5d34db15e675d0a5f366
Toon Spin <toon.spin@gmail.com>
2021-06-05 18:46:10 +0200

    Add and use TerminalVisualizer

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,53 @@ use world::World;
 const WIDTH: usize = 16;
 const HEIGHT: usize = 16;
 
-struct NullVisualizer {}
+struct TerminalVisualizer {
+    width: usize,
+    height: usize,
+    cells: Vec<bool>,
+}
 
-impl Visualizer for NullVisualizer {
-    fn update_world(&self, _: &World) {}
-    fn update_cells(&self, _: &Vec<CellRef>) {}
+impl TerminalVisualizer {
+    fn new(width: usize, height: usize) -> TerminalVisualizer {
+        TerminalVisualizer {
+            width,
+            height,
+            cells: vec![false; width * height],
+        }
+    }
+
+    fn draw_world(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.cells[y * self.width + x] {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    }
+}
+
+impl Visualizer for TerminalVisualizer {
+    fn update_world(&mut self, world: &World) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                self.cells[y * self.width + x] = world.is_alive(x, y);
+            }
+        }
+        self.draw_world();
+    }
+    fn update_cells(&mut self, world: &World, cells: &Vec<CellRef>) {
+        for cell in cells.iter() {
+            let x = cell.x;
+            let y = cell.y;
+            self.cells[y * self.width + x] = world.is_alive(x, y);
+        }
+        println!("{}", String::from('=').repeat(self.width));
+        self.draw_world();
+    }
 }
 
 fn initialize_cell(_x: usize, _y: usize) -> bool {
@@ -19,7 +61,7 @@ fn initialize_cell(_x: usize, _y: usize) -> bool {
 }
 
 fn main() {
-    let visualizer = NullVisualizer {};
+    let mut visualizer = TerminalVisualizer::new(WIDTH, HEIGHT);
     let mut world = World::new(WIDTH, HEIGHT, initialize_cell, &mut visualizer);
     loop {
         world.iterate(&mut visualizer);
