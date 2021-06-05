4ded82766512c57f362a915f1405de23668be0e8
Toon Spin <toon.spin@gmail.com>
2021-06-05 19:41:37 +0200

    Move terminal visualizer to new file, slightly improve it

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,11 @@
 pub mod world;
+pub mod terminalvisualizer;
 
-use world::CellRef;
-use world::Visualizer;
 use world::World;
+use terminalvisualizer::TerminalVisualizer;
 
 const WIDTH: usize = 16;
 const HEIGHT: usize = 16;
-
-struct TerminalVisualizer {
-    width: usize,
-    height: usize,
-    cells: Vec<bool>,
-}
-
-impl TerminalVisualizer {
-    fn new(width: usize, height: usize) -> TerminalVisualizer {
-        TerminalVisualizer {
-            width,
-            height,
-            cells: vec![false; width * height],
-        }
-    }
-
-    fn draw_world(&self) {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                if self.cells[y * self.width + x] {
-                    print!("#");
-                } else {
-                    print!(".");
-                }
-            }
-            println!("");
-        }
-    }
-}
-
-impl Visualizer for TerminalVisualizer {
-    fn update_world(&mut self, world: &World) {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                self.cells[y * self.width + x] = world.is_alive(x, y);
-            }
-        }
-        self.draw_world();
-    }
-    fn update_cells(&mut self, world: &World, cells: &Vec<CellRef>) {
-        for cell in cells.iter() {
-            let x = cell.x;
-            let y = cell.y;
-            self.cells[y * self.width + x] = world.is_alive(x, y);
-        }
-        println!("{}", String::from('=').repeat(self.width));
-        self.draw_world();
-    }
-}
 
 fn initialize_cell(_x: usize, _y: usize) -> bool {
     rand::random::<f32>() < 0.25
---
+++ src/terminalvisualizer.rs
@@ -0,0 +1,52 @@
+use crate::world::{World, Visualizer, CellRef};
+
+pub struct TerminalVisualizer {
+    width: usize,
+    height: usize,
+    cells: Vec<bool>,
+}
+
+impl TerminalVisualizer {
+    pub fn new(width: usize, height: usize) -> TerminalVisualizer {
+        TerminalVisualizer {
+            width,
+            height,
+            cells: vec![false; width * height],
+        }
+    }
+
+    fn draw_world(&self) {
+        let mut chars = Vec::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.cells[y * self.width + x] {
+                    chars.push('#');
+                } else {
+                    chars.push(' ');
+                }
+            }
+            chars.push('\n');
+        }
+        print!("{}", chars.iter().collect::<String>());
+    }
+}
+
+impl Visualizer for TerminalVisualizer {
+    fn update_world(&mut self, world: &World) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                self.cells[y * self.width + x] = world.is_alive(x, y);
+            }
+        }
+        self.draw_world();
+    }
+    fn update_cells(&mut self, world: &World, cells: &Vec<CellRef>) {
+        for cell in cells.iter() {
+            let x = cell.x;
+            let y = cell.y;
+            self.cells[y * self.width + x] = world.is_alive(x, y);
+        }
+        println!("{}", String::from("-").repeat(self.width));
+        self.draw_world();
+    }
+}
